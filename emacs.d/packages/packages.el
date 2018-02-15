b46946b57e5d76fa2ff1ea7fe874dae003274dcf
Josh Bicking <jhb2345@rit.edu>
2018-02-14 21:47:15 -0500

    Add yasnippet

--- emacs.d/packages/packages.el
+++ emacs.d/packages/packages.el
@@ -669,6 +669,30 @@
 
 (require 'prettify-custom-symbols)
 
+(use-package yasnippet
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook #'yas-minor-mode)
+  :config
+  (yas-reload-all)
+  ;; Add yasnippet support for all company backends
+  ;; https://github.com/syl20bnr/spacemacs/pull/179
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends.")
+
+  (defun company-mode/backend-with-yas (backend)
+    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+        backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))
+
+(use-package yasnippet-snippets
+  :ensure t
+  :after yasnippet)
+
+
 ;;;; Optional packages
 
 (use-package flymd
