f7cce1fa8749eae5132d6df67569104f1d246545
Josh Bicking <joshbicking@comcast.net>
2018-03-01 19:54:25 -0500

    prefix with dots

--- rmdots.sh
+++ rmdots.sh
@@ -2,7 +2,7 @@
 
 DIR=$(pwd)
 
-for i in vimrc vim bashrc tmux.conf zshrc emacs.d dircolors compton.conf .config/nvim .antigen.zsh .xmonad .xmobarrc .local/share/applications/emacs-client.desktop
+for i in .vimrc .vim .bashrc .tmux.conf .zshrc.local .zshrc .emacs.d .dircolors .compton.conf .config/nvim .antigen.zsh .xmonad .xmobarrc .local/share/applications/emacs-client.desktop
 do
 	if [ -L $HOME/$i ]
 	then
